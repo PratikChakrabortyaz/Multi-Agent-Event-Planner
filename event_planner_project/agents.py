a537582fe4b8c094eb36461b508cc55f0644d1de
PratikChakrabortyaz <pratik1.mitmpl2022@learner.manipal.edu>
2025-03-22 18:12:49 +0000

    Added Hugging Face API token for authentication

--- event_planner_project/agents.py
+++ event_planner_project/agents.py
@@ -1,10 +1,17 @@
 from smolagents import ToolCallingAgent
 from smolagents import HfApiModel
 from tools import theme_search_tool
+import os
+
+# Load Hugging Face Token
+HF_TOKEN =  user_secrets.get_secret("HF_TOKEN")
 
 theme_agent = ToolCallingAgent(
     tools=[theme_search_tool],
-    model=HfApiModel("Qwen/Qwen2.5-Coder-32B-Instruct"),
+    model=HfApiModel(
+        "Qwen/Qwen2.5-Coder-32B-Instruct",
+        token=HF_TOKEN   # Add the token here
+    ),
     max_steps=5,
     name="Theme Agent",
     description="Finds creative and trending party themes."
